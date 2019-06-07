f1e9c6c13688b8f7f61bb6671e03ed22800bf082
Lorenzo L. Romero <lorenzolrom@gmail.com>
2019-06-07 09:40:07 -0400

    Skeleton files.

---
+++ src/public/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * LLR Technologies & Associated Services
+ * Information Systems Development
+ *
+ * ISD MERCURY DOORWAY
+ *
+ * URL Shortener
+ *
+ * User: lromero
+ * Date: 6/07/2019
+ * Time: 9:39 AM
+ */
