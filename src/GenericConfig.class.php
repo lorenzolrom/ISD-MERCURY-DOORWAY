342858a33523f10e513b7daab6aca2b09ed684db
Lorenzo L. Romero <lorenzolrom@gmail.com>
2019-06-07 09:44:45 -0400

    Generic configureation file

---
+++ src/GenericConfig.class.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * LLR Technologies & Associated Services
+ * Information Systems Development
+ *
+ * ISD MERCURY DOORWAY
+ *
+ * URL Shortener
+ *
+ * User: lromero
+ * Date: 6/07/2019
+ * Time: 9:40 AM
+ */
+
+
+class GenericConfig
+{
+    const OPTIONS = array(
+        'appName' => 'URL Shortener',
+
+        'baseURL' => 'example.com',
+
+        'icURL' => 'https://service.com/service/',
+        'icSecret' => ''
+    );
+}
